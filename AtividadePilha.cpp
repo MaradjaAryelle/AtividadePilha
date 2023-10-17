a8190ff9dfdca3513b6d4ddf0896d58e0d2332b9
Maradja Aryelle <125324662+MaradjaAryelle@users.noreply.github.com>
2023-10-17 16:38:29 -0300

    Add files via upload

--- AtividadePilha.cpp
+++ AtividadePilha.cpp
@@ -10,6 +10,7 @@ struct Cidade{
 struct ElementoPilha{
 	Cidade cidade;
 	ElementoPilha *next;
+	char caractere;
 };
 
 void exibirLista(ElementoPilha *topo){
@@ -56,6 +57,64 @@ void removerMeio(ElementoPilha *&topo, int meio) {
     anterior->next = temporario->next;
     delete temporario;
     }
+    
+void desempilhar(ElementoPilha *&topo, ElementoPilha *&temp) {
+    if (topo == NULL) {
+        return;
+    }
+
+    temp = topo;
+    topo = topo->next;
+
+    desempilhar(topo, temp->next);
+
+    if (topo != NULL) {
+        ElementoPilha *tempTopo = topo;
+        while (tempTopo->next) {
+            tempTopo = tempTopo->next;
+        }
+        tempTopo->next = temp;
+        temp->next = NULL;
+    } else {
+        topo = temp;
+    }
+}
+
+void reverterPilha(ElementoPilha *&topo) {
+    ElementoPilha *temp = NULL;
+    desempilhar(topo, temp);
+}
+
+void empilharPalavra(ElementoPilha*& topo, char c){
+    ElementoPilha* novoElemento = new ElementoPilha;
+    novoElemento->caractere = c;
+    novoElemento->next = topo;
+    topo = novoElemento;
+}
+
+char desempilharPalavra(ElementoPilha*& topo){
+    if(topo == NULL) return '\0';
+    char c = topo->caractere;
+    ElementoPilha* temp = topo;
+    topo = topo->next;
+    delete temp;
+    return c;
+}
+
+string reverterString(string palavra) {
+    ElementoPilha* topo = NULL;
+    
+    for(char c : palavra) {
+        empilharPalavra(topo, c);
+    }
+    
+    string revertida = "";
+    char c;
+    while((c = desempilharPalavra(topo)) != '\0') {
+        revertida += c;
+    }
+    return revertida;
+}
 
 int main(int argc, char** argv) {
 	int meio;
@@ -68,11 +127,24 @@ int main(int argc, char** argv) {
 	empilhar(topo, "Campina", 250000);
 	
 	meio = acharMeio(topo);
-	cout << meio << endl;
-
+	
+	cout << "A lista inicial eh:";
+	exibirLista(topo);
+	
 	removerMeio(topo, meio);
+	cout << "\n\nO elemento do meio foi removido e a nova lista eh:";
 	exibirLista(topo);
 
+	reverterPilha(topo);
+	cout << "\n\nA lista foi revertida para:";
+	exibirLista(topo);
 
+	string palavra;
+	cout << "\n\nDigite uma palavra:";
+	cin >> palavra;
+	
+	string revertida =  reverterString(palavra);
+	cout << "Palavra revertida: " << revertida << endl;
+	
 	return 0;
 }
